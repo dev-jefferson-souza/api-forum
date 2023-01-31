65358d99d3ccbb4b23979414d4884edd1108f938
Jefferson Souza <jefferson.souza@neki-it.com.br>
2023-01-31 09:30:53 -0300

    feat(Test):CursoRepositoryTest

--- src/main/java/br/com/alura/forum/config/security/DevSecurityConfigurations.java
+++ src/main/java/br/com/alura/forum/config/security/DevSecurityConfigurations.java
@@ -19,8 +19,8 @@ import br.com.alura.forum.repository.UsuarioRepository;
 
 @EnableWebSecurity
 @Configuration
-@Profile("prod")
-public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
+@Profile("dev")
+public class DevSecurityConfigurations extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	private AutenticacaoService autenticacaoService;
@@ -47,22 +47,8 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-		.antMatchers(HttpMethod.GET, "/topicos").permitAll()
-		.antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
-		.antMatchers(HttpMethod.POST, "/auth").permitAll()
-		.antMatchers(HttpMethod.GET, "/actuator/**").permitAll()
-		.antMatchers(HttpMethod.DELETE, "/topicos/*").hasRole("MODERADOR")
-		.anyRequest().authenticated()
-		.and().csrf().disable()
-		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-		.and().addFilterBefore(new AutenticacaoViaTokenFilter(tokenService, usuarioRepository), UsernamePasswordAuthenticationFilter.class);
-	}
-	
-	
-	//Configuracoes de recursos estaticos(js, css, imagens, etc.)
-	@Override
-	public void configure(WebSecurity web) throws Exception {
-		web.ignoring().antMatchers("/**.html", "/h2-console/**", "/v2/api-docs", "/webjars/**", "/configuration/**", "/swagger-resources/**");
+		.antMatchers("/**").permitAll()
+		.and().csrf().disable();
 	}
 	
 }
